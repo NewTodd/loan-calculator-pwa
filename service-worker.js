a8b9d0d5f30e3f91d43bea3c400f29eeca8fc328
todd-code <toddvong090@gmail.com>
2025-05-31 12:37:38 -0500

    Fix service worker cache issue and update UI

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('loan-cache-v1').then((cache) => {
+    caches.open('loan-cache-v2').then((cache) => {
       return cache.addAll([
         './',
         './index.html',
